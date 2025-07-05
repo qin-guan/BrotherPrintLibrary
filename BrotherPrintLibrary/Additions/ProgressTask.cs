0aacbb615cbed0bfdc2c20b4a59e5cc3402de8fe
Qin Guan <qinguan20040914@gmail.com>
2025-07-05 22:12:47 +0800

    Initial commit

---
+++ BrotherPrintLibrary/Additions/ProgressTask.cs
@@ -0,0 +1,15 @@
+namespace Com.Brother.Ptouch.Sdk
+{
+
+  public sealed unsafe partial class ProgressTask : global::Java.Lang.Object
+  {
+    ~ProgressTask()
+    {
+			const string __id = "finalize.()V";
+			try {
+				_members.InstanceMethods.InvokeNonvirtualVoidMethod (__id, this, null);
+			} finally {
+			}
+    }
+  }
+}
---
+++ BrotherPrintLibrary/Additions/RJPrintSettings.cs
@@ -0,0 +1,37 @@
+namespace Com.Brother.Sdk.Lmprinter.Setting
+{
+
+  public partial class RJPrintSettings
+  {
+
+    public global::Com.Brother.Sdk.Lmprinter.Setting.IPrintImageSettings.Halftone? GetHalftone()
+    {
+      return Halftone;
+    }
+
+    public global::Com.Brother.Sdk.Lmprinter.Setting.IPrintImageSettings.PrintQuality? GetPrintQuality()
+    {
+      return PrintQuality;
+    }
+
+    public global::Com.Brother.Sdk.Lmprinter.Setting.IPrintImageSettings.ScaleMode? GetScaleMode()
+    {
+      return ScaleMode;
+    }
+
+    public void SetHalftone(global::Com.Brother.Sdk.Lmprinter.Setting.IPrintImageSettings.Halftone? p0)
+    {
+      Halftone = p0;
+    }
+
+    public void SetPrintQuality(global::Com.Brother.Sdk.Lmprinter.Setting.IPrintImageSettings.PrintQuality? p0)
+    {
+      PrintQuality = p0;
+    }
+
+    public void SetScaleMode(global::Com.Brother.Sdk.Lmprinter.Setting.IPrintImageSettings.ScaleMode? p0)
+    {
+      ScaleMode = p0;
+    }
+  }
+}
